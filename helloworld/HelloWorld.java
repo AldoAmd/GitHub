22078de1c2e85ca5fae7eb1f85ec433f70db43a0
AldoAmd <shafakhandieng@gmail.com>
2017-01-25 12:03:24 +0100

    che\eclipse
    beginning...

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the .
+ */
+package helloworld;
+
+/**
+ *
+ * @author AmadouDieng
+ */
+//import java.util.Scanner; 
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        // TODO code application logic her
+//        //System.out.print("hello,World !");
+//        System.out.print(" je m'appelle");
+//        System.out.print(" Amadou hahah\n");
+//    import java.util.Scanner;   
+/*Scanner sc = new Scanner(System.in);
+
+System.out.println("Veuillez saisir un mot :");
+
+int str = sc.nextInt();
+
+System.out.println("Vous avez saisi : " + str);*/
+        double i = (int)1000000;
+        System.out.println(i);
+    }
+}
